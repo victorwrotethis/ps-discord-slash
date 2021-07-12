efd6bbba4e71d2506fd2e4bf3b6cb699b2183a97
Victor <vdcommits@outlook.com>
2021-07-13 00:10:02 +0200

    Add to temp global builder

--- ps-discord-slash-python/main/gecore/ps_discord_slash/implementations/load_global_commands.py
+++ ps-discord-slash-python/main/gecore/ps_discord_slash/implementations/load_global_commands.py
@@ -1,3 +1,4 @@
+from gecore.ps_discord_slash.commands.command_manager.command_manager_command import CommandManagerSlashCommand
 from gecore.ps_discord_slash.commands.models.local_commands import SlashCommand, SlashCommands
 from gecore.ps_discord_slash.commands.models.local_permissions import CommandPermissions
 from gecore.ps_discord_slash.implementations.unplug.unplug_command import UnplugSlashCommand
@@ -7,4 +8,6 @@ def load_global_commands() -> SlashCommands:
     global_perms = CommandPermissions(allowed_users=[97325654688681984])
     unplug_command = UnplugSlashCommand.build()
     sb_unplug_command = SlashCommand(unplug_command, global_perms)
-    return SlashCommands([sb_unplug_command])
+    management_command = CommandManagerSlashCommand.build()
+    sb_management_command = SlashCommand(management_command, global_perms)
+    return SlashCommands([sb_unplug_command, sb_management_command])
