de101be70e18bb2cc829360e49dc90bfe8e1e27b
Victor <vdcommits@outlook.com>
2022-03-04 23:55:09 +0100

    added oshur bases

--- ps-discord-slash-python/main/gecore/ps_discord_slash/implementations/bases/extra_bases.py
+++ ps-discord-slash-python/main/gecore/ps_discord_slash/implementations/bases/extra_bases.py
@@ -1,5 +1,3 @@
-from enum import IntEnum
-
 
 def additional_bases():
     """"Manually added bases that cannot be retrieved from the REST API"""
@@ -8,54 +6,349 @@ def additional_bases():
             "facility_name": "Koltyr Northern Warpgate",
             "facility_id": 400014,
             "facility_type_id": 7,
-            "facility_type": "Warpgate"
+            "facility_type": "Warpgate",
+            "zone_id": 131086
         },
         {
             "facility_name": "Koltyr Eastern Warpgate",
             "facility_id": 400015,
             "facility_type_id": 7,
-            "facility_type": "Warpgate"
+            "facility_type": "Warpgate",
+            "zone_id": 131086
         },
         {
             "facility_name": "Koltyr Southern Warpgate",
             "facility_id": 400016,
             "facility_type_id": 7,
-            "facility_type": "Warpgate"
+            "facility_type": "Warpgate",
+            "zone_id": 131086
         },
         {
             "facility_name": "Zorja",
             "facility_id": 400017,
             "facility_type_id": 2,
-            "facility_type": "Amp Station"
+            "facility_type": "Amp Station",
+            "zone_id": 131086
         },
         {
             "facility_name": "Xander",
             "facility_id": 400018,
             "facility_type_id": 3,
-            "facility_type": "Bio Lab"
+            "facility_type": "Bio Lab",
+            "zone_id": 131086
         },
         {
             "facility_name": "Svarog",
             "facility_id": 400019,
             "facility_type_id": 4,
-            "facility_type": "Tech Plant"
+            "facility_type": "Tech Plant",
+            "zone_id": 131086
         },
         {
             "facility_name": "Koltyr Tech Plant Outpost",
             "facility_id": 400020,
             "facility_type_id": 5,
-            "facility_type": "Large Outpost"
+            "facility_type": "Large Outpost",
+            "zone_id": 131086
         },
         {
             "facility_name": "Koltyr Biolab Outpost",
             "facility_id": 400021,
             "facility_type_id": 5,
-            "facility_type": "Large Outpost"
+            "facility_type": "Large Outpost",
+            "zone_id": 131086
         },
         {
             "facility_name": "Koltyr Amp Station Outpost",
             "facility_id": 400022,
             "facility_type_id": 5,
+            "facility_type": "Large Outpost",
+            "zone_id": 131086
+        },
+        {
+            "name": "Mirror Bay Watchtower",
+            "facility_id": 400329,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Astira Hydroelectric",
+            "facility_id": 400330,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Pommel Gardens",
+            "facility_id": 400331,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Seapost J5",
+            "facility_id": 400390,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Centri Import Hub",
+            "facility_id": 400332,
+            "zone_id": 334,
+            "facility_type_id": 9,
+            "facility_type": "Construction Outpost"
+        }, {
+            "name": "Seapost K10",
+            "facility_id": 400391,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Binusilan Interlink",
+            "facility_id": 400333,
+            "zone_id": 334,
+            "facility_type_id": 8
+        }, {
+            "name": "Seapost K12",
+            "facility_id": 400392,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Ligalai Station",
+            "facility_id": 400334,
+            "zone_id": 334,
+            "facility_type_id": 9,
+            "facility_type": "Construction Outpost"
+        }, {
+            "name": "Outpost Kalis",
+            "facility_id": 400335,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Seaside Bluffs",
+            "facility_id": 400336,
+            "zone_id": 334,
+            "facility_type_id": 9,
+            "facility_type": "Construction Outpost"
+        }, {
+            "name": "Viridian Genetics Lab",
+            "facility_id": 400337,
+            "zone_id": 334,
+            "facility_type_id": 5,
             "facility_type": "Large Outpost"
+        }, {
+            "name": "Viridian Decontamination",
+            "facility_id": 400338,
+            "zone_id": 334,
+            "facility_type_id": 9,
+            "facility_type": "Construction Outpost"
+        }, {
+            "name": "Centri Mining Operation",
+            "facility_id": 400339,
+            "zone_id": 334,
+            "facility_type_id": 5,
+            "facility_type": "Large Outpost"
+        }, {
+            "name": "Mirror Bay Checkpoint",
+            "facility_id": 400340,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Pilay Interlink",
+            "facility_id": 400341,
+            "zone_id": 334,
+            "facility_type_id": 8
+        }, {
+            "name": "Gildad Cliffs",
+            "facility_id": 400346,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Lamplight Security",
+            "facility_id": 400342,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Hopeswell Beachhead",
+            "facility_id": 400343,
+            "zone_id": 334,
+            "facility_type_id": 9,
+            "facility_type": "Construction Outpost"
+        }, {
+            "name": "Wavecrest Beachhead",
+            "facility_id": 400344,
+            "zone_id": 334,
+            "facility_type_id": 9,
+            "facility_type": "Construction Outpost"
+        }, {
+            "name": "Veridad Pass",
+            "facility_id": 400404,
+            "zone_id": 334,
+            "facility_type_id": 9,
+            "facility_type": "Construction Outpost"
+        }, {
+            "name": "Anat Interlink",
+            "facility_id": 400345,
+            "zone_id": 334,
+            "facility_type_id": 8
+        }, {
+            "name": "Solstice Pass",
+            "facility_id": 400405,
+            "zone_id": 334,
+            "facility_type_id": 9,
+            "facility_type": "Construction Outpost"
+        }, {
+            "name": "Sage R&D Labs",
+            "facility_id": 400347,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Tannae Power",
+            "facility_id": 400348,
+            "zone_id": 334,
+            "facility_type_id": 5,
+            "facility_type": "Large Outpost"
+        }, {
+            "name": "Wakerift Beachhead",
+            "facility_id": 400407,
+            "zone_id": 334,
+            "facility_type_id": 9,
+            "facility_type": "Construction Outpost"
+        }, {
+            "name": "Hunter's Ravine",
+            "facility_id": 400349,
+            "zone_id": 334,
+            "facility_type_id": 9,
+            "facility_type": "Construction Outpost"
+        }, {
+            "name": "K&H Tech Station",
+            "facility_id": 400350,
+            "zone_id": 334,
+            "facility_type_id": 9,
+            "facility_type": "Construction Outpost"
+        }, {
+            "name": "High Ridge Security",
+            "facility_id": 400351,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Dekat Interlink",
+            "facility_id": 400352,
+            "zone_id": 334,
+            "facility_type_id": 8
+        }, {
+            "name": "Astira Solar Station",
+            "facility_id": 400353,
+            "zone_id": 334,
+            "facility_type_id": 9,
+            "facility_type": "Construction Outpost"
+        }, {
+            "name": "Southpeak Meadows",
+            "facility_id": 400354,
+            "zone_id": 334,
+            "facility_type_id": 9,
+            "facility_type": "Construction Outpost"
+        }, {
+            "name": "Emerald Research Co.",
+            "facility_id": 400355,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Remnant Cove",
+            "facility_id": 400356,
+            "zone_id": 334,
+            "facility_type_id": 9,
+            "facility_type": "Construction Outpost"
+        }, {
+            "name": "Viridian East Terrace",
+            "facility_id": 400357,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "South Viridian Beachhead",
+            "facility_id": 400358,
+            "zone_id": 334,
+            "facility_type_id": 9,
+            "facility_type": "Construction Outpost"
+        }, {
+            "name": "Terran Genetics Inc.",
+            "facility_id": 400359,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Sibo Interlink",
+            "facility_id": 400360,
+            "zone_id": 334,
+            "facility_type_id": 8
+        }, {
+            "name": "Wellerman Watch",
+            "facility_id": 400361,
+            "zone_id": 334,
+            "facility_type_id": 5,
+            "facility_type": "Large Outpost"
+        }, {
+            "name": "Palaso Supply Depot",
+            "facility_id": 400362,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Fort Arceo",
+            "facility_id": 400363,
+            "zone_id": 334,
+            "facility_type_id": 5,
+            "facility_type": "Large Outpost"
+        }, {
+            "name": "Eagle Outpost",
+            "facility_id": 400364,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Meridian Listening Post",
+            "facility_id": 400365,
+            "zone_id": 334,
+            "facility_type_id": 9,
+            "facility_type": "Construction Outpost"
+        }, {
+            "name": "Imbanon Interlink",
+            "facility_id": 400366,
+            "zone_id": 334,
+            "facility_type_id": 8
+        }, {
+            "name": "Nascent Shipping and Storage",
+            "facility_id": 400367,
+            "zone_id": 334,
+            "facility_type_id": 6,
+            "facility_type": "Small Outpost"
+        }, {
+            "name": "Emerald Arboretum",
+            "facility_id": 400368,
+            "zone_id": 334,
+            "facility_type_id": 5,
+            "facility_type": "Large Outpost"
+        }, {
+            "name": "Sirinan Trident",
+            "facility_id": 400372,
+            "zone_id": 334,
+            "facility_type_id": 12
+        }, {
+            "name": "Bago Trident",
+            "facility_id": 400373,
+            "zone_id": 334,
+            "facility_type_id": 12
+        }, {
+            "name": "Anlabban Trident",
+            "facility_id": 400374,
+            "zone_id": 334,
+            "facility_type_id": 12
         }
     ]
