7a3d7a345e33172ec18f5f201cd5379b4f343a53
Victor <vdcommits@outlook.com>
2022-03-24 17:46:40 +0100

    Disabled search-base for time being

--- ps-discord-slash-python/main/gecore/ps_discord_slash/implementations/bases/search_base_command.py
+++ ps-discord-slash-python/main/gecore/ps_discord_slash/implementations/bases/search_base_command.py
@@ -1,18 +1,20 @@
 from gecore.ps_discord_slash.commands.command_interface import InteractionCommand, InteractionCommandType, StartingPerms
 from gecore.ps_discord_slash.implementations.created_commands import OvOInteractionCommand
-from gecore.ps_discord_slash.implementations.bases.models.base_response import create_base_response
-from gecore.ps_discord_slash.implementations.bases.parse_bases import BasesParser
-from gecore.ps_discord_slash.implementations.bases.search_bases import find_bases
+# from gecore.ps_discord_slash.implementations.bases.models.base_response import create_base_response
+# from gecore.ps_discord_slash.implementations.bases.parse_bases import BasesParser
+# from gecore.ps_discord_slash.implementations.bases.search_bases import find_bases
 from gecore.ps_discord_slash.models.commands import ApplicationCommand, ApplicationCommandOption, \
     ApplicationCommandOptionType
 from gecore.ps_discord_slash.models.discord_config import GenericConfig
-from gecore.ps_discord_slash.models.interactions import InteractionResponse
+from gecore.ps_discord_slash.models.flags import DiscordFlags
+from gecore.ps_discord_slash.models.interactions import InteractionResponse, InteractionResponseData, \
+    InteractionResponseType
 
 
 class SearchBaseSlashCommand(InteractionCommand):
 
-    def __init__(self):
-        self.base_list = BasesParser().create_base_list()
+    #def __init__(self):
+        #self.base_list = BasesParser().create_base_list()
 
     @staticmethod
     def starting_perms() -> StartingPerms:
@@ -42,6 +44,11 @@ class SearchBaseSlashCommand(InteractionCommand):
         )
 
     def execute(self, command_body: {}) -> InteractionResponse:
-        search_arg = command_body['data']['options'][0]['value']
-        base_result = find_bases(self.base_list, search_arg)
-        return create_base_response(base_result, search_arg)
+        # search_arg = command_body['data']['options'][0]['value']
+        # base_result = find_bases(self.base_list, search_arg)
+        # return create_base_response(base_result, search_arg)
+        data = InteractionResponseData(
+            content=f'Lost made me disable this :(',
+            flags=DiscordFlags.HIDDEN
+        )
+        return InteractionResponse(InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE, data)
